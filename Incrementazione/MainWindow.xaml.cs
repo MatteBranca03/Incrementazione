d9d880e98b5fef692525b025fd1bc80c340796ff
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-13 12:23:32 +0100

    cambiamenti

--- Incrementazione/MainWindow.xaml.cs
+++ Incrementazione/MainWindow.xaml.cs
@@ -24,5 +24,91 @@ namespace Incrementazione
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 * 0.001;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 * 0.01;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 * 0.1;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 + 1;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 + 0.1;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 + 0.01;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 / 0.001;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 / 0.01;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 / 0.01;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 - 1;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 - 0.1;
+            txtnumero.Text = n.ToString();
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            double n1 = double.Parse(txtnumero.Text);
+            double n = n1 - 0.01;
+            txtnumero.Text = n.ToString();
+
+        }
     }
+    
 }
